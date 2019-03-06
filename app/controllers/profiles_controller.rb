f0c4e3193027bf95c67e04ca93e535ac4ee9dc9a
Xavier Pichetti <pichon.pich@free.fr>
2019-03-06 17:11:56 +0100

    Ajout de la page profile avec modification de l'adresse et de la page mes plantes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,8 +6,27 @@ class ProfilesController < ApplicationController
   end
 
   def show
+    @orders = current_user.profile.orders.order(created_at: :desc)
+  end
+
+  def my_plants
+    @orders = current_user.profile.orders.order(created_at: :asc)
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
   end
 
   def update
+    @profile = Profile.find(params[:id])
+    @profile.update(profile_params)
+    redirect_to profile_path(@profile)
   end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:last_name, :first_name, :address, :address_city, :address_zipcode, :address_country)
+  end
+
 end
