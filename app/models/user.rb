2201b8ecd7d81973bed6b2d08ec5bc03cc62bf2e
Xavier Pichetti <pichon.pich@free.fr>
2019-03-08 14:49:43 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :profile, dependent: :destroy
-  accepts_nested_attributes_for :profile
+  
 end
