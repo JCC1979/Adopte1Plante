07af1a6704b30a2d1ed05d25f68bee5e785189dd
Antoine Potdevin <antoinepotdevin@gmail.com>
2019-03-12 16:56:38 +0100

    The page is fully dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   helper_method :current_or_guest_user
   def home
+    @plants = Plant.all.limit(1)
     session[:cart_guest] = false
   end
 end
