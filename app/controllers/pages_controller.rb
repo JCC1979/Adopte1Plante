3cc1d33493faf2a525580d917dd823c0cfb4d3ec
JC COEVOET <jcc197959@hotmail.fr>
2019-03-14 16:38:18 +0100

    recherche effective en hp

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,21 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   helper_method :current_or_guest_user
   def home
-    @plants = Plant.all.sample(Plant.all.size)
+    if params["choices-multiple-remote-fetch"]
+      searchselect = params["choices-multiple-remote-fetch"]
+      searchtable = searchselect.split(/ /)
+
+      if searchtable.length == 1
+        @plants = Plant.searchgenderonly(searchselect) + Plant.all
+        @plants = @plants.uniq
+      else
+        @plants = Plant.searchtotal(searchselect) + Plant.searchgenderonly(searchtable[0]) + Plant.all
+        @plants = @plants.uniq
+      end
+      
+    else
+      @plants = Plant.all.sample(Plant.all.size)
+    end
     session[:cart_guest] = false
   end
 end
